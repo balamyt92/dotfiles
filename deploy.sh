9d09ccf188f99fd0dedb27294488d4791cb39316
Anatoly Medvedev <balamyt92@gmail.com>
2016-10-26 22:54:04 +0700

    fix deploy script Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#### Install minimall soft
+
+echo 'Установка пакетов программного обеспечения (необходимы root права)...'
+apt install -y zsh zsh-syntax-highlighting vim htop atop nomacs virtualbox-qt vlc qbittorrent
+
+
 #### Install configs
 
 echo 'Установка настроек и создание симлинков...\n'
@@ -7,8 +13,3 @@ for file in $(find ~/dotfiles | grep init.sh)
 do
    bash $file
 done
-
-#### Install minimall soft
-
-echo 'Установка пакетов программного обеспечения (необходимы root права)...'
-apt install -y zsh zsh-syntax-highlighting vim htop atop nomacs virtualbox-qt vlc qbittorrent
