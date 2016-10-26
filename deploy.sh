87f5ee6bde3205076ecb0465867f35144ae8f5d6
Anatoly Medvedev <balamyt92@gmail.com>
2016-10-26 22:13:30 +0700

    add install soft

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+#### Install configs
+
+echo 'Установка настроек и создание симлинков...\n'
 for file in $(find ~/dotfiles | grep init.sh)
 do
    bash $file
 done
+
+#### Install minimall soft
+
+echo 'Установка пакетов программного обеспечения (необходимы root права)...'
+apt install -y zsh zsh-syntax-highlighting vim htop atop nomacs virtualbox-qt vlc qbittorrent
