ec8769fc668bab7f4ab6302a74989072af324840
Anatoly Medvedev <balamyt92@gmail.com>
2016-10-27 11:42:54 +0700

    fix zsh clear config

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~/
 rm -rf ~/.zsh*
+rm -rf ~/.oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
