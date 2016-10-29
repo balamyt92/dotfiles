94c082a9142733c32f11d211883f70f7c6733fee
Anatoly Medvedev <balamyt92@gmail.com>
2016-10-29 23:15:36 +0700

    rename zshrc

--- zsh/init.sh
+++ zsh/init.sh
@@ -3,4 +3,4 @@ cd ~/
 rm -rf ~/.zsh*
 rm -rf ~/.oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
